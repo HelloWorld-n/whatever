4c461105900d9082ca65a5da3b5968f69a050252
HelloWorld-n <gajica.b@gmail.com>
2025-03-02 16:19:36 +0100

    sort in different ways

--- src/go_gin_pages/sort.go
+++ src/go_gin_pages/sort.go
@@ -1,6 +1,7 @@
 package go_gin_pages
 
 import (
+	"math"
 	"net/http"
 	"sync"
 
@@ -9,16 +10,73 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func receiveSort(c *gin.Context) {
+func incrementalSort(c *gin.Context) {
 	var wg sync.WaitGroup
-	var arr []int
+	var arr []float64
 	c.ShouldBindBodyWithJSON(&arr)
 	wg.Add(1)
-	sorting.SimpleSort(arr, &wg)
+	sorting.SimpleSort(
+		arr,
+		func(a0 float64, a1 float64) bool {
+			return a0 < a1
+		},
+		&wg,
+	)
+	wg.Wait()
+	c.JSON(http.StatusOK, arr)
+}
+
+func decrementalSort(c *gin.Context) {
+	var wg sync.WaitGroup
+	var arr []float64
+	c.ShouldBindBodyWithJSON(&arr)
+	wg.Add(1)
+	sorting.SimpleSort(
+		arr,
+		func(a0 float64, a1 float64) bool {
+			return a0 > a1
+		},
+		&wg,
+	)
+	wg.Wait()
+	c.JSON(http.StatusOK, arr)
+}
+
+func absoluteIncrementalSort(c *gin.Context) {
+	var wg sync.WaitGroup
+	var arr []float64
+	c.ShouldBindBodyWithJSON(&arr)
+	wg.Add(1)
+	sorting.SimpleSort(
+		arr,
+		func(a0 float64, a1 float64) bool {
+			return math.Abs(a0) < math.Abs(a1)
+		},
+		&wg,
+	)
+	wg.Wait()
+	c.JSON(http.StatusOK, arr)
+}
+
+func absoluteDecrementalSort(c *gin.Context) {
+	var wg sync.WaitGroup
+	var arr []float64
+	c.ShouldBindBodyWithJSON(&arr)
+	wg.Add(1)
+	sorting.SimpleSort(
+		arr,
+		func(a0 float64, a1 float64) bool {
+			return math.Abs(a0) > math.Abs(a1)
+		},
+		&wg,
+	)
 	wg.Wait()
 	c.JSON(http.StatusOK, arr)
 }
 
 func prepareSort(route *gin.RouterGroup) {
-	route.POST("", receiveSort)
+	route.POST("", incrementalSort)
+	route.POST("/reverse", decrementalSort)
+	route.POST("/abs", absoluteIncrementalSort)
+	route.POST("/abs-reverse", absoluteDecrementalSort)
 }
--- src/utils/sorting/sort.go
+++ src/utils/sorting/sort.go
@@ -2,14 +2,15 @@ package sorting
 
 import (
 	"sync"
-
-	"golang.org/x/exp/constraints"
 )
 
-func JoinSortedArrays[T constraints.Ordered](arr0, arr1 []T) (result []T) {
+func JoinSortedArrays[T any](
+	arr0, arr1 []T,
+	cmp func(T, T) bool,
+) (result []T) {
 	result = make([]T, 0)
 	for len(arr0) > 0 && len(arr1) > 0 {
-		if arr0[0] < arr1[0] {
+		if cmp(arr0[0], arr1[0]) {
 			result = append(result, arr0[0])
 			arr0 = arr0[1:]
 		} else {
@@ -22,17 +23,21 @@ func JoinSortedArrays[T constraints.Ordered](arr0, arr1 []T) (result []T) {
 	return
 }
 
-func SimpleSort[T constraints.Ordered](arr []T, wg *sync.WaitGroup) {
+func SimpleSort[T any](
+	arr []T,
+	cmp func(T, T) bool,
+	wg *sync.WaitGroup,
+) {
 	defer wg.Done()
 	var innerWg sync.WaitGroup
 	if len(arr) < 2 {
 		return
 	}
 	innerWg.Add(2)
-	go SimpleSort(arr[:len(arr)/2], &innerWg)
-	go SimpleSort(arr[len(arr)/2:], &innerWg)
+	go SimpleSort(arr[:len(arr)/2], cmp, &innerWg)
+	go SimpleSort(arr[len(arr)/2:], cmp, &innerWg)
 	innerWg.Wait()
-	for i, v := range JoinSortedArrays(arr[:len(arr)/2], arr[len(arr)/2:]) {
+	for i, v := range JoinSortedArrays(arr[:len(arr)/2], arr[len(arr)/2:], cmp) {
 		arr[i] = v
 	}
 }
